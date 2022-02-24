8f1ea0b4ff4f5993501bb396ac4662cb8646b254
Mostafa <m.n.irib@gmail.com>
2022-02-24 23:08:31 +0330

    added dist 0.0.7

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ classifiers = [
   'Programming Language :: Python :: 3.8'
 ]
 
-VERSION = '0.0.6'
+VERSION = '0.0.7'
 DESCRIPTION = 'Tradingview scraper tool'
 
 # Setting up
@@ -23,7 +23,7 @@ setup(
     author="Mostafa Najmi",
     author_email="m.n.irib@gmail.com",
     url = 'https://github.com/mnwato/tradingview-scraper',
-    download_url = 'https://github.com/mnwato/tradingview-scraper/archive/refs/tags/0.0.6.tar.gz',
+    download_url = 'https://github.com/mnwato/tradingview-scraper/archive/refs/tags/0.0.7.tar.gz',
     description=DESCRIPTION,
     long_description_content_type="text/markdown",
     long_description=readme(),
