1ce2821cba9b0912ae0e3f7de7518272b253450d
Mostafa <m.n.irib@gmail.com>
2022-03-11 16:28:07 +0330

    added build

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+ 
+classifiers = [
+  'Development Status :: 2 - Pre-Alpha',
+  'Intended Audience :: Developers',
+  'Operating System :: OS Independent',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3.8'
+]
+
+VERSION = '0.1.2'
+DESCRIPTION = 'Tradingview scraper tool'
+
+# Setting up
+setup(
+    name="tradingview-scraper",
+    version=VERSION,
+    author="Mostafa Najmi",
+    author_email="m.n.irib@gmail.com",
+    url = 'https://github.com/mnwato/tradingview-scraper',
+    download_url = 'https://github.com/mnwato/tradingview-scraper/archive/refs/tags/0.1.0.zip',
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=readme(),
+    License = 'MIT',
+    packages=find_packages(),
+    install_requires=['requests', 'pandas', 'beautifulsoup4', 'numpy'],
+    keywords=['tradingview', 'scraper', 'python', 'crawler', 'financial'],
+    classifiers=classifiers
+)
