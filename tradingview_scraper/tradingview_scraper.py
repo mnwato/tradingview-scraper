d790d2509dbea41256bb764824586f96a14ad347
Mostafa <m.n.irib@gmail.com>
2022-06-02 12:49:18 +0430

    bug fixed: data paths were not work in pypi package >> stored datas inside of code. update release version

--- tradingview_scraper/tradingview_scraper.py
+++ tradingview_scraper/tradingview_scraper.py
@@ -217,18 +217,16 @@ class Ideas:
                 df.to_csv(f"tradingview_{symbol}.csv", index=False)
 
             return description, df
-import os
+
 class Indicators:
     def __init__(self):
 
         # Read and save all indicators
-        with open(os.path.join(os.getcwd(), 'tradingview_scraper/data/indicators.txt'), "r") as f:
-            self.indicators = f.read().replace('"', "").split(",")
-            f.close()
+        indicators = """"Recommend.Other","Recommend.All","Recommend.MA","RSI","RSI[1]","Stoch.K","Stoch.D","Stoch.K[1]","Stoch.D[1]","CCI20","CCI20[1]","ADX","ADX+DI","ADX-DI","ADX+DI[1]","ADX-DI[1]","AO","AO[1]","AO[2]","Mom","Mom[1]","MACD.macd","MACD.signal","Rec.Stoch.RSI","Stoch.RSI.K","Rec.WR","W.R","Rec.BBPower","BBPower","Rec.UO","UO","EMA10","close","SMA10","EMA20","SMA20","EMA30","SMA30","EMA50","SMA50","EMA100","SMA100","EMA200","SMA200","Rec.Ichimoku","Ichimoku.BLine","Rec.VWMA","VWMA","Rec.HullMA9","HullMA9","Pivot.M.Classic.S3","Pivot.M.Classic.S2","Pivot.M.Classic.S1","Pivot.M.Classic.Middle","Pivot.M.Classic.R1","Pivot.M.Classic.R2","Pivot.M.Classic.R3","Pivot.M.Fibonacci.S3","Pivot.M.Fibonacci.S2","Pivot.M.Fibonacci.S1","Pivot.M.Fibonacci.Middle","Pivot.M.Fibonacci.R1","Pivot.M.Fibonacci.R2","Pivot.M.Fibonacci.R3","Pivot.M.Camarilla.S3","Pivot.M.Camarilla.S2","Pivot.M.Camarilla.S1","Pivot.M.Camarilla.Middle","Pivot.M.Camarilla.R1","Pivot.M.Camarilla.R2","Pivot.M.Camarilla.R3","Pivot.M.Woodie.S3","Pivot.M.Woodie.S2","Pivot.M.Woodie.S1","Pivot.M.Woodie.Middle","Pivot.M.Woodie.R1","Pivot.M.Woodie.R2","Pivot.M.Woodie.R3","Pivot.M.Demark.S1","Pivot.M.Demark.Middle","Pivot.M.Demark.R1"""
+        self.indicators = indicators.replace('"', "").split(",")
 
-        with open(os.path.join(os.getcwd(), 'tradingview_scraper/data/exchanges.txt'), "r") as f:
-            self.exchanges = f.read().split(",")
-            f.close()
+        exchanges = "BINANCE,BINANCEUS,BITCOKE,BITFINEX,BITSTAMP,BITTREX,BYBIT,CAPITALCOM,CEXIO,CURRENCYCOM,EASYMARKETS,EIGHTCAP,EXMO,FOREXCOM,FTX,FXCM,GEMINI,GLOBALPRIME,INDEX,KRAKEN,OANDA,OKCOIN,PEPPERSTONE,SAXO,SKILLING,TIMEX,TRADESTATION,VANTAGE"
+        self.exchanges = exchanges.split(",")
 
     def scraper(
         self,
